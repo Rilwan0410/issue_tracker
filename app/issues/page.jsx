927e900e30ab5db66bfdc8774b585e75362e0b16
Rilwan0410 <rilwan410@hotmail.com>
2025-02-04 10:10:17 -0500

    added radix ui to project

--- app/issues/page.jsx
+++ app/issues/page.jsx
@@ -1,9 +1,9 @@
-
+import { Button } from "@radix-ui/themes"
 
 export default function IssuesPage() {
   return (
   <div>
-
+<Button>Hello</Button>
     </div>
     )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
+import { Theme } from "@radix-ui/themes";
 import Navbar from "./Navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        <main>{children}</main>
+        <Theme>
+          <Navbar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
