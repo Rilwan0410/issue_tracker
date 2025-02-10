4b05df0027f56c4e85c77368fcebc319b47d9b61
Rilwan0410 <rilwan410@hotmail.com>
2025-02-09 20:25:34 -0500

    added assignedUderId to API

--- app/api/issues/[id]/route.js
+++ app/api/issues/[id]/route.js
@@ -1,19 +1,26 @@
 import { NextResponse } from "next/server";
-import { createIssueSchema } from "../../../validationSchema";
+import { createIssueSchema ,patchIssueSchema} from "../../../validationSchema";
 import { prisma } from "../../../../prisma/client";
 import { getServerSession } from "next-auth";
 
 export async function PATCH(req, { params: { id } }) {
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
-  
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
+
   const body = await req.json();
   console.log(body);
 
-  const validation = createIssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 201 });
 
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+    if (!user)
+      return NextResponse({ error: "User does not exist" }, { status: 400 });
+  }
   const issue = await prisma.issue.findUnique({ where: { id: Number(id) } });
 
   if (!issue)
@@ -24,15 +31,19 @@ export async function PATCH(req, { params: { id } }) {
 
   const editedIssue = await prisma.issue.update({
     where: { id: Number(id) },
-    data: { title: body.title, description: body.description },
+    data: {
+      title: body.title,
+      description: body.description,
+      assignedToUserId: body.assignedToUserId,
+    },
   });
 
   return NextResponse.json(editedIssue, { status: 201 });
 }
 
 export async function DELETE(req, { params: { id } }) {
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
   const issue = await prisma.issue.findUnique({ where: { id: Number(id) } });
   if (!issue)
     return NextResponse.json(
--- app/validationSchema.js
+++ app/validationSchema.js
@@ -1,6 +1,24 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const createIssueSchema = z.object({
   title: z.string().min(1, { message: "Title is required." }).max(255),
   description: z.string().min(1, { message: "Description is required." }),
+});
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Title is required." })
+    .max(255)
+    .optional(),
+  description: z
+    .string()
+    .min(1, { message: "Description is required." })
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, { message: "AssignedTouserId is required" })
+    .max(255)
+    .optional()
+    .nullable(),
 });
