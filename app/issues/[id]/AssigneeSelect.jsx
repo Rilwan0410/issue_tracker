3c47c95e080a7d858c8ac92f7665cdd69e426c9c
Rilwan0410 <rilwan410@hotmail.com>
2025-02-10 04:19:59 -0500

    show toast notifications

--- app/issues/[id]/AssigneeSelect.jsx
+++ app/issues/[id]/AssigneeSelect.jsx
@@ -2,33 +2,45 @@
 import { Select } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function AssigneeSelect({ users, issue }) {
   const router = useRouter();
   async function updateUser(userId) {
-    await axios.patch(`/api/issues/${issue.id}`, { assignedToUserId: userId });
-    router.push("/issues");
+    try {
+      await axios.patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId,
+      });
+      // router.push("/issues");
+      if (userId !== null) toast("Succesfully assigned issue");
+      else toast("Succesfully unassigned issue");
+    } catch (error) {
+      toast.error("Changes could not be saved");
+    }
   }
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || ''}
-      onValueChange={(userId) => updateUser(userId)}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value={null}>Unassign</Select.Item>
-          {users.map((user, i) => {
-            return (
-              <Select.Item key={i} value={user.id}>
-                {user.name}
-              </Select.Item>
-            );
-          })}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={(userId) => updateUser(userId)}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value={null}>Unassign</Select.Item>
+            {users.map((user, i) => {
+              return (
+                <Select.Item key={i} value={user.id}>
+                  {user.name}
+                </Select.Item>
+              );
+            })}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 }
--- app/issues/[id]/updateUser.js
+++
@@ -1,7 +0,0 @@
-import { prisma } from "../../../prisma/client";
-import axios from "axios";
-async function updateUser(userId, issue) {
-  //   const user = await prisma.user.findUnique({ where: { id: userId } });
-
-  await axios.patch(`/api/issues/${issue.id}`, { assingedToUser: userId });
-}
